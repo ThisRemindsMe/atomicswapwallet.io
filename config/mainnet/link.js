d2b0ca9c1c5aebcaf458929a4fd661aa6294e9fa
Pavel Khavilov <43392281+Pasha8914@users.noreply.github.com>
2020-04-03 15:41:01 +1000

    Update link.js

--- config/mainnet/link.js
+++ config/mainnet/link.js
@@ -1,8 +1,11 @@
 export default {
   xlm: 'http://stellarchain.io',
   bitpay: 'https://insight.bitpay.com',
+  blockcypher: 'https://live.blockcypher.com/',
   etherscan: 'https://etherscan.io',
   omniexplorer: 'https://www.omniexplorer.info',
+  bch: 'https://explorer.bitcoin.com/bch',
   ltc: 'https://insight.litecore.io',
   usdt: 'https://omniexplorer.info/',
+  qtum: 'https://explorer.qtum.org/',
 }
