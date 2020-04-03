ffb48f1d6abccdaec1c46cc9fddbd6de7e8e98cf
Pavel Khavilov <43392281+Pasha8914@users.noreply.github.com>
2020-04-03 15:42:17 +1000

    Update api.js

--- config/testnet/api.js
+++ config/testnet/api.js
@@ -2,10 +2,12 @@ export default {
   horizon: 'https://horizon-testnet.stellar.org',
   blocktrail: 'https://api.blocktrail.com/v1/tBTC',
   // bitpay: 'https://test-insight.Atomicswapwallet.io/insight-api',
-  bitpay: 'https://test-insight.bitpay.com/api',
+  bitpay: 'https://test-insight.swaponline.io/insight-api',
   etherscan: 'https://rinkeby.etherscan.io/api',
+  etherscan_ApiKey: 'RHHFPNMAZMD6I4ZWBZBF6FA11CMW9AXZNM',
   usdt: 'https://api.omniexplorer.info/',
   ltc: 'https://testnet.litecore.io/api',
   swapsExplorer: 'https://seven.Atomicswapwallet.io/swaps-testnet/state',
   faucet: 'https://seven.Atomicswapwallet.io/faucet/testnet/faucet/',
 }
+
