8b6d6a12d2f8a2611ac3ebeca6ee71d7eca3b328
Pavel Khavilov <43392281+Pasha8914@users.noreply.github.com>
2020-04-03 15:43:14 +1000

    Update link.js

--- config/testnet/link.js
+++ config/testnet/link.js
@@ -1,8 +1,11 @@
 export default {
   xlm: 'http://testnet.stellarchain.io',
-  // bitpay: 'https://test-insight.Atomicswapwallet.io/insight',
-  bitpay: 'https://test-insight.bitpay.com',
+  bitpay: 'https://www.blockchain.com/ru/btctest',//'https://test-insight.swap.online/insight',
+  // bitpay: 'https://test-insight.bitpay.com',
   etherscan: 'https://rinkeby.etherscan.io',
+  bch: 'https://explorer.bitcoin.com/tbch',
   ltc: 'https://testnet.litecore.io',
+  qtum: 'https://explorer.qtum.org/',
+  // usdtOmni: 'https://omniexplorer.info/',
   usdt: 'https://omniexplorer.info/',
 }
